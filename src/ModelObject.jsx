0ee25c0ae563ae926f6c8dd14ebd7221c94601d6
Roberto Sintos <sintosroberto3@gmail.com>
2025-11-10 10:00:13 +0800

    update

--- src/ModelObject.jsx
+++ src/ModelObject.jsx
@@ -5,7 +5,12 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 {
   /*import { useLoader } from "@react-three/fiber";*/
 }
-import { AnimationMixer, LoadingManager, TextureLoader } from "three";
+import {
+  AnimationMixer,
+  AxesHelper,
+  LoadingManager,
+  TextureLoader,
+} from "three";
 import { OrbitControls } from "@react-three/drei";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
@@ -13,14 +18,42 @@ import gsap from "gsap";
 import "./style/ModelObject.css";
 import LoadingBar from "./pages/Loading";
 
+{
+  /*
+
 const LogoImage = () => {
   const texture = useLoader(TextureLoader, "/logo_white.png");
   return (
     <mesh position={[0, -13.8, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-      <planeGeometry args={[40, 40]} />
+      <planeGeometry args={[50, 50]} />
       <meshBasicMaterial map={texture} transparent={true} alphaTest={0.5} />
     </mesh>
   );
+};
+
+*/
+}
+
+const CameraScroll = () => {
+  const scrollY = useRef(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      scrollY.current = window.scrollY * 0.01;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  useFrame(({ camera }) => {
+    camera.position.z = 8 + scrollY.current * 0.5;
+    camera.position.y = scrollY.current * 1;
+    camera.lookAt(0, 0, 0);
+  });
+
+  return null;
 };
 
 const Model = ({ setProgress, onLoaded }) => {
@@ -57,35 +90,31 @@ const Model = ({ setProgress, onLoaded }) => {
 
   if (!gltf) return null;
   return (
-    <primitive object={gltf.scene} scale={[5, 5, 5]} position={[0, -13.7, 3]} />
+    <primitive object={gltf.scene} scale={[5, 5, 5]} position={[6, -14.7, 5]} />
   );
-};
-
-const CameraScroll = () => {
-  const scrollY = useRef(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      scrollY.current = window.scrollY * 0.01;
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
-  useFrame(({ camera }) => {
-    camera.position.z = 8 + scrollY.current * 0.5;
-    camera.position.y = scrollY.current * 1;
-    camera.lookAt(0, 0, 0);
-  });
-
-  return null;
 };
 
 const ModelObject = () => {
   const [progress, setProgress] = useState(0);
   const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const html = document.documentElement;
+    const body = document.body;
+
+    if (!loaded) {
+      html.style.overflow = "hidden";
+      body.style.overflow = "hidden";
+    } else {
+      html.style.overflow = "auto";
+      body.style.overflow = "auto";
+    }
+
+    return () => {
+      html.style.overflow = "auto";
+      body.style.overflow = "auto";
+    };
+  }, [loaded]);
 
   useGSAP(() => {
     gsap.fromTo(
@@ -107,17 +136,19 @@ const ModelObject = () => {
     <>
       {!loaded && <LoadingBar progress={progress} />}
       <div className="model-wrapper">
-        <Canvas camera={{ position: [0, 10, 10], fov: 80 }} dpr={[1, 1.5]}>
-          <ambientLight intensity={3} color="green" />
+        <Canvas camera={{ position: [14, 10, 5], fov: 80 }} dpr={[1, 1.5]}>
+          <ambientLight intensity={3} color="black" />
           <directionalLight position={[0, 25, 20]} intensity={2} />
-          <spotLight intensity={5} position={[0, 15, 1]} color="green" />
+          <spotLight intensity={5} position={[0, 15, 1]} color="white" />
           <Model setProgress={setProgress} onLoaded={() => setLoaded(true)} />
-          <LogoImage />
+
+          {/*      <LogoImage />
+           */}
           <CameraScroll />
         </Canvas>
 
         <div className="hero-div">
-          <h2 id="hero-text"> Scan Tronix</h2>
+          {/*    <h3 id="hero-text">ScanTronix</h3>*/}
         </div>
       </div>
 
